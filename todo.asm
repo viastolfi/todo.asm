d4d2092eec18fae47fb5968fefc40653f3cd74ef
viastolfi <vincent.astolfi12@gmail.com>
2026-04-16 13:58:13 +0200

    feat: create file.db and add strcat function

--- todo.asm
+++ todo.asm
@@ -4,7 +4,13 @@ section .data
   usage_error_msg db "Usage : ./todo db", 10
   usage_error_len equ $ - usage_error_msg
 
+  file_opening_error_msg db "Error while opening db file", 10
+  file_opening_error_len equ $ - file_opening_error_msg
+
+  file_extension db ".db", 0
+
 section .bss
+  db_name resb 256
 
 section .text
 global _start
@@ -16,8 +22,17 @@ _start:
   jl _usage_error
 
   ;get db name and check if file exist
-  mov rbx, [rsp + 16]
+  mov rsi, [rsp + 16]
+  mov rdi, db_name
+  call _strcpy
 
+  mov rbx, db_name
+  ;add .db extension to db file
+  call _strcat
+
+  mov rbx, rax
+
+  ;open/create db file
   mov rax, SYS_OPEN
   mov rdi, rbx
   mov rsi, O_CREAT|O_RDWR|O_TRUNC
@@ -25,8 +40,7 @@ _start:
   syscall
 
   cmp rax, 0
-  ; TODO: change this with on file open error
-  jl _usage_error
+  jl _file_error
 
   mov rbx, rax
   mov rax, SYS_CLOSE
@@ -34,6 +48,46 @@ _start:
   syscall
 
   call _program_end
+
+_strcpy:
+copy:
+  mov al, [rsi]
+  mov [rdi], al
+  cmp al, 0
+  je done
+  inc rsi
+  inc rdi
+  jmp copy
+done:
+  ret
+
+_strcat:
+  mov rdi, rbx ; keep the beggining of the string in rdi
+  mov rsi, file_extension
+parse:
+  cmp byte [rbx], 0
+  je concat
+  inc rbx
+  jmp parse
+concat:
+  cmp byte[rsi], 0
+  je finish
+  mov cl, byte[rsi]
+  mov byte[rbx], cl
+  inc rbx
+  inc rsi
+  jmp concat
+finish:
+  mov rax, rdi ; return the beggining of the string
+  ret
+
+_file_error:
+  mov rax, SYS_WRITE
+  mov rdi, 1
+  mov rsi, file_opening_error_msg
+  mov rdx, file_opening_error_len
+  syscall
+  call _program_end_error
 
 _usage_error:
   mov rax, SYS_WRITE
