c510939dd25172a1fcd218fce2fddd56a76d812e
viastolfi <vincent.astolfi12@gmail.com>
2026-04-17 22:14:50 +0200

    fix: some refactor

--- todo.asm
+++ todo.asm
@@ -1,4 +1,5 @@
 %include "linux.inc"
+%include "utils.inc"
 
 section .data
   usage_error_msg db "Usage : ./todo db", 10
@@ -25,8 +26,8 @@ _start:
   jl _usage_error
 
   ;get db name and check if file exist
-  mov rsi, [rsp + 16]
   mov rdi, db_name
+  mov rsi, [rsp + 16]
   call _strcpy
 
 .db_file_opening:
@@ -40,11 +41,7 @@ _start:
   mov [rsp + 8], rax
 
   ;open db file
-  mov rax, SYS_OPEN
-  mov rdi, [rsp + 8]
-  mov rsi, O_RDWR
-  mov rdx, 0
-  syscall
+  syscall3 SYS_OPEN, [rsp + 8], O_RDWR, 0
 
   ;check if db file exist or not
   cmp rax, 0
@@ -54,66 +51,14 @@ _start:
 .db_file_reading:
   ;store the file descriptor
   mov [rsp + 16], rax
-  ;read the content of the file and print it
-  mov rax, SYS_READ
-  mov rdi, [rsp + 16]
-  mov rsi, buffer
-  mov rdx, 256
-  syscall
+  ;read the content of the file
+  syscall3 SYS_READ, [rsp + 16], buffer, 256
 
-  mov rdi, buffer
-  call _strlen
-  mov rbx, rax
-
+.enable_raw_mode:
   call _program_end
 
-;;  int strlen(char* s)
-_strlen:
-  xor rcx, rcx
-len:
-  cmp byte[rdi + rcx], 0
-  je len_done
-  inc rcx
-  jmp len
-len_done:
-  mov rax, rcx
-  ret 
+_main_loop:
 
-;; void strcpy(char* src, char* dest)
-_strcpy:
-copy:
-  mov al, [rsi]
-  mov [rdi], al
-  cmp al, 0
-  je done
-  inc rsi
-  inc rdi
-  jmp copy
-done:
-  ret
-
-;; char* strcat(char* s1, char* s2)
-;; copy s2 at the end of s1
-_strcat:
-  mov rdi, rbx ; keep the beggining of the string in rdi
-  mov rsi, file_extension
-parse:
-  cmp byte [rbx], 0
-  je concat
-  inc rbx
-  jmp parse
-concat:
-  cmp byte[rsi], 0
-  je finish
-  mov cl, byte[rsi]
-  mov byte[rbx], cl
-  inc rbx
-  inc rsi
-  jmp concat
-finish:
-  mov rax, rdi ; return the beggining of the string
-  ret
- 
 ; Check if a file error is due to file not existing
 ; Create it if so
 ; Exit with error otherwise
@@ -124,41 +69,23 @@ _file_error_handling:
   cmp rdi, 2
   jne _file_error
   
-  mov rax, SYS_OPEN
-  mov rdi, rsi
-  mov rsi, O_RDWR|O_CREAT|O_TRUNC
-  mov rdx, S_IRUSR|S_IWUSR
-  syscall
+  syscall3 SYS_OPEN, rsi, O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR
   ret
 
 _file_error:
-  mov rax, SYS_WRITE
-  mov rdi, 1
-  mov rsi, file_opening_error_msg
-  mov rdx, file_opening_error_len
-  syscall
+  syscall3 SYS_WRITE, 1, file_opening_error_msg, file_opening_error_len
   call _program_end_error
 
 _usage_error:
-  mov rax, SYS_WRITE
-  mov rdi, 1
-  mov rsi, usage_error_msg
-  mov rdx, usage_error_len
-  syscall
+  syscall3 SYS_WRITE, 1, usage_error_msg, usage_error_len
   call _program_end_error
 
 _program_end_error:
-  mov rax, SYS_EXIT
-  mov rdi, 1
-  syscall
+  syscall1 SYS_EXIT, 1
 
 _program_end:
   ;TODO: maybe store db_fd in bss section to avoid stack usage error
   ;close the file
-  mov rax, SYS_CLOSE
-  mov rdi, [rsp + 16]
-  syscall
+  syscall1 SYS_CLOSE, [rsp + 16]
 
-  mov rax, SYS_EXIT
-  mov rdi, 0
-  syscall 
+  syscall1 SYS_EXIT, 0
